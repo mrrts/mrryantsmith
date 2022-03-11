36816c901b0107f60c059647e35d2a28fd0582da
Ryan Smith <mrryantsmith@gmail.com>
2022-03-11 09:15:36 -0600

    add contact page content

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,14 @@
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Head from "next/head";
 import Layout from "../components/Layout";
 import { useEntranceAnimation } from "../lib/hooks/useEntranceAnimation.hook";
+import styles from '../styles/pages/contact.module.css';
 
 const Contact = () => {
   useEntranceAnimation('.contact > *');
-  
+
   return (
     <Layout>
       <Head>
@@ -13,6 +17,32 @@ const Contact = () => {
       
       <div className='contact'>
         <h2>Contact</h2>
+
+        <p className={styles.intro}>
+          If you would like to get in touch, please visit my LinkedIn profile, or find my contact details on my resume.  
+        </p>
+
+        <div className={styles.resumeLink}>
+          <a
+            href="/resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon className={styles.icon} icon={faDownload} />
+            Download Resume
+          </a>
+        </div>
+
+        <div className={styles.linkedInLink}>
+          <a
+            href="https://linkedin.com/in/mrryantsmith"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon className={styles.icon} icon={faLinkedin} />
+            LinkedIn Profile
+          </a>
+        </div>
       </div>
     </Layout>
   );
